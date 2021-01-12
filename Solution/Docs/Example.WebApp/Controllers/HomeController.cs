6d41411cd8cbc99ac6795aa36795f234f3425f6c
To Ka <me@toka.ge>
2021-01-12 19:08:14 +0400

    Update docs and add example

---
+++ Solution/Docs/Example.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using BankOfGeorgia.IpayClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Example.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BankOfGeorgiaIpayClient _iPayClient;
+
+        public HomeController(
+            BankOfGeorgiaIpayClient IpayClient
+            )
+        {
+            _iPayClient = IpayClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Pay()
+        {
+            //Create Order
+            var order = new BankOfGeorgiaIpayOrder()
+            {
+
+            };
+
+            // Create Transaction
+            var registerResult = await _iPayClient
+                .MakeOrderAsync(order);
+
+            // Check if error occured
+
+
+            // When succeeded redirect to bank page
+            var redirectUrl = "";
+
+            return Redirect(redirectUrl);
+        }
+    }
+}
---
+++ Solution/Docs/Example.WebApp/Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddBankOfGeorgiaIpay(
+                Configuration.GetTbcBankEcommerceOptions("Bog")
+            );
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
