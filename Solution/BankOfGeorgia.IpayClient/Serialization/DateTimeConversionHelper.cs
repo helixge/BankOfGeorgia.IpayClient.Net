c99b991ce8dbc579cca33387da61fd3a2337d75e
zakareishvili <nodar@helix.ge>
2022-01-13 13:03:03 +0400

    datetime converter

---
+++ Solution/BankOfGeorgia.IpayClient/Serialization/DateTimeConversionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankOfGeorgia.IpayClient
+{
+    public static class DateTimeConversionHelper
+    {
+        private const string _ipayDateFormat = "ddd MMM dd HH:mm:ss 'GET' yyyy";
+
+        public static DateTimeOffset ConvertIpayStringToDateTimeOffset(string value)
+        {
+            return DateTime.ParseExact(value, _ipayDateFormat, CultureInfo.InvariantCulture);
+        }
+    }
+}
