1d6349e1727caaa79c9080d9b5b240987485d1f5
To Ka <me@toka.ge>
2022-03-20 23:47:09 +0400

    Add StringDecimalConversionHelperTests TODOs

---
+++ Solution/Testing/BankOfGeorgia.IpayClient.Tests/Serialization/StringDecimalConversionHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfGeorgia.IpayClient.Tests.Serialization
+{
+    public class StringDecimalConversionHelperTests
+    {
+        //TODO:
+
+        public void ConvertDecimalToString_WhenNoDecimalPoints_DoesNotAppend0()
+        {
+
+        }
+
+        public void ConvertDecimalToString_WhenDecimalPoints_ContainsOnlyOneDot()
+        {
+
+        }
+
+        public void ConvertDecimalToString_WhenGreaterThanThousand_DoesNotContainSpaces()
+        {
+
+        }
+
+        public void ConvertStringToDecimal_WhenContainsThousandsSeparatorSpace_Converts()
+        {
+
+        }
+
+        public void ConvertStringToDecimal_WhenContainsDecimalSepratorComma_Converts()
+        {
+
+        }
+
+        public void ConvertStringToDecimal_WhenContainsDecimalSepratorDot_Converts()
+        {
+
+        }
+    }
+}
