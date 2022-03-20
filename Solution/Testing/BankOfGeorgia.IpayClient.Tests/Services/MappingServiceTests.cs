70ca957305d0a26a404f958bde3fb8ee3d77c544
To Ka <me@toka.ge>
2022-03-20 23:49:25 +0400

    Add MappingServiceTests TODO

---
+++ Solution/Testing/BankOfGeorgia.IpayClient.Tests/Services/MappingServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfGeorgia.IpayClient.Tests.Services
+{
+    public class MappingServiceTests
+    {
+        // TODO:
+
+        public void CreateIpayOrderRequest_MapsAppValues()
+        {
+
+        }
+    }
+}
