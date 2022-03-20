54825a62b5ab660a6fa2eaaaf2ffb42a4215e21e
To Ka <me@toka.ge>
2022-03-20 23:52:14 +0400

    Add BankOfGeorgiaIpayClientOptionsTests

---
+++ Solution/Testing/BankOfGeorgia.IpayClient.Tests/BankOfGeorgiaIpayClientOptionsTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace BankOfGeorgia.IpayClient.Tests
+{
+    public class BankOfGeorgiaIpayClientOptionsTests
+    {
+        [Fact]
+        public void BankOfGeorgiaIpayClientOptions_ConstructoParamatersAreAssigned()
+        {
+            // Arrange
+            string clientId = Guid.NewGuid().ToString();
+            string secretKey = Guid.NewGuid().ToString();
+
+            // Act
+            var options = new BankOfGeorgiaIpayClientOptions(clientId, secretKey);
+
+            // Assert
+            options.ClientId.Should().Be(clientId);
+            options.SecretKey.Should().Be(secretKey);
+        }
+    }
+}
